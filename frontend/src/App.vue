9fc200742b507b411b538b214e9c0ed318fed6a0
Gennadiy Karpov <asgardkarpov@yandex.ru>
2024-06-01 16:00:49 +1000

    added patient info component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,38 @@
   <p>Пациент</p>
   <p>Период</p>
 </header>
-  <brain-map />
+<body>
+  <div class="container">
+    <div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    Действие
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Действие</a></li>
+    <li><a class="dropdown-item" href="#">Другое действие</a></li>
+    <li><a class="dropdown-item" href="#">Что-то еще здесь</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Отделенная ссылка</a></li>
+  </ul>
+</div>
+<div class="container">
+    <div class="row align-items-start">
+    <div class="col">
+      <brain-map />
+    </div>
+    <div class="col">
+      <patient-info />
+    </div>
+    </div>
+  </div>
+</div>
+</body>
 </template>
 
 <script setup >
 import { ref , createApp} from "vue";
 import BrainMap from './components/BrainMap'
+import PatientInfo from './components/PatientInfo'
 const app = createApp({})
 
 // Регистрация компонента
---
+++ frontend/src/components/PatientInfo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container back-container" style="padding: 0%;">
+        <div class="container back-container-info">
+            <!-- тут надо данные подсасывать с бэкенда -->
+            <h3>Данные о пациенте</h3>
+            ФИО/АЙДИ: <br>
+            Пол: <br>
+            Рост: <br>
+            Вес: <br>
+            Пульс: <br>
+            Другие данные: <br>
+        </div>
+        <div class="container back-container-info">
+            <h3>Результат анализа ЭЭГ на АПНОЭ</h3>
+            <!-- тут надо данные подсасывать с бэкенда -->
+            Вероятность АПНОЭ: 74%
+        </div>
+        
+
+    </div>
+  </template>
+  
+  <script setup >
+  
+  </script>
+  
+  
+<style lang="scss">
+ .back-container {
+    background-color: #e0fbfc;
+    min-height: 80vh;
+    border: 2px solid #cecece;
+ }
+
+ .back-container-info {
+    background-color: #e0fbfc;
+    min-height: 40vh;
+    border: 2px solid #cecece;
+ }
+
+
+ .back-table {
+    background-color: beige;
+ }
+</style>
+  
